8545f92bc22927f18ec25b82ba5325f28a15ce68
Max Nesterov <braams@braams.ru>
2017-02-15 19:08:05 +0300

    add link to rtpengine

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # txrtpengine
 Twisted based rtpengine client (Proof of concept)
+
+Aimed to work with [Sipwise rtpengine](https://github.com/sipwise/rtpengine) over ng Control Protocol (NGCP)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='txrtpengine',
+      version='0.1',
+      description='Twisted based rtpengine client',
+      author='Max Nesterov',
+      author_email='braams@braams.ru',
+      url='https://github.com/braams/txrtpengine/',
+      packages=['txrtpengine',],
+     )
